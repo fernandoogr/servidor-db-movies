49e46e6cb1e8bf32212efec7ec29e5af9f1be1b3
Fernando <fernando.06cm@gmail.com>
2026-01-20 10:05:39 +0100

    crear movies.db dentro de /db

--- db/init.ts
+++ db/init.ts
@@ -2,7 +2,7 @@ import { Database } from "bun:sqlite"
 import { getAllMovies } from "../models/movies"
 
 export async function initDB(){
-    const db = new Database("movies.db")
+    const db = new Database("./db/movies.db")
     const schema: string = await Bun.file("./db/schema.sql").text()
     db.run(schema)
     const peliculas = getAllMovies(db)
