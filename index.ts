db907a09e7b7e7ef35b1d9b7502071c92bee8feb
Fernando <fernando.06cm@gmail.com>
2026-01-20 11:28:16 +0100

    añadimos endpoint

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
 import { initDB } from './db/init';
-import { getAllMovies } from './models/movies';
+import { getAllMovies, getMovieById, getMoviesByName } from './models/movies';
+import express from 'express';
 
+const app = express();
+const PORT = 3000;
 const db = await initDB();
 
-const movies = getAllMovies(db);
-console.log(movies);
+app.get("/movies", (req, res) => {
+    const peliculas = getAllMovies(db);
+    res.json(peliculas);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server corriendo en el puerto º${PORT}`);
+});
+
+
+//const movies = getAllMovies(db);
+//console.log(movies);
+
+//const movie = getMovieById(db, 1);
+//console.log(movie);
+
+//const moviesByName = getMoviesByName(db, 'Matrix');
+//console.log(moviesByName);
--- models/movies.ts
+++ models/movies.ts
@@ -3,3 +3,13 @@ export function getAllMovies(db: Database) {
     const query = db.query("SELECT * FROM peliculas")
     return query.all()
 }
+
+export function getMovieById(db: Database, id: number) {
+    const query = db.query("SELECT * FROM peliculas WHERE id = ?")
+    return query.get(id)
+}
+
+export function getMoviesByName(db: Database, name: string) {
+    const query = db.query("SELECT * FROM peliculas WHERE nombre LIKE ?")
+    return query.all(`%${name}%`)
+}
